f5afd98146d58b073a8a60eaa0e9fedf1526c947
Rikard Fahlström <rikard.fahlstrom@gmail.com>
2017-09-29 06:22:35 +0200

    adopted to new html-structure

--- apps/05_weather_client/program.py
+++ apps/05_weather_client/program.py
@@ -2,7 +2,8 @@ import requests
 import bs4
 import collections
 
-WeatherReport = collections.namedtuple('WeatherReport','cond, temp, scale, loc')
+WeatherReport = collections.namedtuple('WeatherReport', 'cond, temp, scale, loc')
+
 
 def main():
     print_the_header()
@@ -16,26 +17,30 @@ def main():
         report.cond
     ))
 
+
 def print_the_header():
     print('------------------------------')
     print('          WEATHER APP')
     print('------------------------------')
 
+
 def get_html_from_web(zipcode):
     url = 'https://www.wunderground.com/weather-forecast/{}'.format(zipcode)
-    # print(url)
+    #  print(url)
     response = requests.get(url)
-    # print(response.status_code)
-    # print(response.text[0:250])
+    #  print(response.status_code)
+    #  print(response.text[0:250])
+
     return response.text
 
-def get_weather_from_html(html):
-    soup = bs4.BeautifulSoup(html, "html5lib")
 
-    loc = soup.find(id="location").find("h1").get_text()
-    condition = soup.find(id="curCond").find(class_="wx-value").get_text()
-    temp = soup.find(id="curTemp").find(class_="wx-value").get_text()
-    unit = soup.find(id="curTemp").find(class_="wx-unit").get_text()
+def get_weather_from_html(html):
+    soup = bs4.BeautifulSoup(html, "html.parser")
+
+    loc = soup.find(class_='region-content-header').find('h1').get_text()
+    condition = soup.find(class_="condition-icon").get_text()
+    temp = soup.find(class_="wu-unit-temperature").find(class_="wu-value").get_text()
+    unit = soup.find(class_="wu-unit-temperature").find(class_="wu-label").get_text()
 
     loc = cleanup_text(loc)
     loc = find_city_and_state_from_loc(loc)
@@ -43,18 +48,24 @@ def get_weather_from_html(html):
     temp = cleanup_text(temp)
     unit = cleanup_text(unit)
 
-    #print(condition, temp, unit, loc)
+    #  print(condition, temp, unit, loc)
     report = WeatherReport(cond=condition, temp=temp, scale=unit, loc=loc)
+
     return report
+
 
 def cleanup_text(text):
     if not text:
         return text
     text = text.strip()
+
     return text
 
-def find_city_and_state_from_loc(loc : str): # ': str' anger att input kommer alltid vara en str. Detta gör inbyggda "hjälpfunktioner tillgängliga i autocompletion.
-    parts = loc.split('\n') # Detta kommer separera texten vid varje nu rad och spara varje del i en lista i 'parts'-variabeln
+
+def find_city_and_state_from_loc(loc: str):  # ':str' in input variable defines that function input will
+    # always be a string. This enables some special string-functionality in the function, like '.split()'
+    parts = loc.split('\n')  # This will split the text at each new line, and save each line
+    # as a separate input in the 'parts'-variable
     return parts[0].strip()
 
 
